638c069790838daf57b906f555125c4b8bbacec6
SeongriPark <tjdfl0516@gmail.com>
2025-05-19 17:51:37 +0900

    초기 환경 세팅 : .gitignore 추가

---
+++ src/main/java/com/ohgiraffers/tomatolab_imean/TomatolabImeanApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.tomatolab_imean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatolabImeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomatolabImeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohgiraffers/tomatolab_imean/TomatolabImeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.tomatolab_imean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomatolabImeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
